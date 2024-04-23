b1eb40993d3c7f3104ed5d56c1a586230f085976
SajaD SaeeDi <thunder11like@gmail.com>
2024-04-23 04:29:49 +0330

    [Change] test title name in config

--- config/seo.php
+++ config/seo.php
@@ -7,7 +7,7 @@ return [
     'viewport'        => "width=device-width, initial-scale=1",
     'description'     => "default description here...",
     'keywords'        => "default, keywords, here",
-    'author'          => "هشتگ چاپ",
+    'author'          => "your author name",
     'robots'          => config('app.env') == 'production' ? "index, follow" : "noindex, nofollow",
     'image'           => "https://url.to/image.png",
     'icon'            => "favicon.ico",
